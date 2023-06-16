e732adedae2ab36a688a2380e589eda9fb25f0f9
kefifaiez <124183820+kefifaiez@users.noreply.github.com>
2023-06-16 23:02:10 +0100

    Update index.js

--- TP1/Backend/index.js
+++ TP1/Backend/index.js
@@ -12,20 +12,3 @@ server.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
 
-// Route pour la connexion d'un utilisateur
-app.post('/users/login', (req, res) => {
-  const { username, password } = req.body;
-
-  connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, rows) => {
-    if (err) {
-      console.error('Erreur lors de l\'exécution de la requête:', err.message);
-      res.status(500).json({ message: 'Erreur lors de la vérification des informations de connexion' });
-    } else {
-      if (rows.length > 0) {
-        res.json({ message: 'Utilisateur connecté' });
-      } else {
-        res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect' });
-      }
-    }
-  });
-});
